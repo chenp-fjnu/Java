afd35c3bc2ae296ef2721f5c3f5bdebbeefffaac
chenp-fjnu <chp0524@gmail.com>
2017-03-25 20:42:05 +0800

    researching on Spring boot.

---
+++ contacts/src/main/java/contacts/HelloController.java
@@ -0,0 +1,12 @@
+package contacts;
+
+@Controller
+@EnableAutoConfiguration
+public class HelloController {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
