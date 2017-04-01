58d131dc74e3c0aa3368d2cbb7aa0dadf97cfad5
chenp-fjnu <chp0524@gmail.com>
2017-04-01 16:39:54 +0800

    initial springcloud demo project

---
+++ springclouddemo/src/main/java/com/nfc/springclouddemo/App.java
@@ -0,0 +1,13 @@
+package com.nfc.springclouddemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
